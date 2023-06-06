7edce4bc3fe8b86febd5e0b7b69398d10fc20683
Nikiforov Dmitry <dmitriynkf86@gmail.com>
2023-06-06 22:13:30 +0500

    ... Добавил большую пачку методов в класс Formula и прописал тесты к ним

--- objects.py
+++ objects.py
@@ -14,38 +14,107 @@ ETWorker = namedtuple('ETWorker', ('title', 'quan', 'salary'))
 Worker = namedtuple('Worker', ('basic_worker', 'auxiliary_worker', 'et_worker'))
 
 class Formula:
-    """
-    Для работы с формулами и их значениями
-    """
+    """Для работы с формулами и их значениями"""
     def __init__(self, workers: tuple):
         # кортеж с объектами описывающих работников
         self.workers = workers
-        # число рабочих
+        # общее число рабочих
         self.quan_workers = sum(worker.quan for worker in workers)
 
         # Сср.чт - среднечасовая тарифная ставка
-        self.hourly_rate = 0
+        self.hourly_rate = None
         # Зтар - тарифная оплата труда
-        self.tariff_salary = 0
+        self.tariff_salary = None
         # Дн - доплата за работу в ночное время
-        self.surch_night_time = 0
+        self.surch_night_time = None
+        # Др.з. - доплата за работу по расширенной зоне
+        self.surch_extended_zone = None
+        # Дпр.в - доплата за работу в праздничные и выходные дни
+        self.surch_public_holidays = None
+        # Дст.р. - доплата за стаж работы
+        self.surch_work_experience = None
+        # Д - сумма всех доплат
+        self.amount_surchs = None
+        # Зпр - прямая оплата труда
+        self.direct_wages = None
+        # Пр - сумма премий
+        self.amount_premium = None
+        # РК - сумма доплат по районному коэффициенту
+        self.amount_surchs_DC = None
+        # СН - северная надбавка
+        self.amount_surchs_NA = None
+        # Зосн - основная оплата труда
+        self.basic_salary = None
+        # Здоп - дополнительная оплата труда
+        self.additional_salary = None
+        # Зобщ - общая оплата труда
+        self.total_salary = None
+        # Зср.м - среднемесячная зарплата
+        self.amount_salary = None
 
     def calc_hourly_rate(self) -> None:
-        """
-        Вычисляет среднечасовую тарифную ставку по бригаде = Сср.чт
-        """
+        """Вычисляет среднечасовую тарифную ставку по бригаде = Сср.чт"""
         result = sum(worker.quan * worker.tariff_rate for worker in self.workers)
         self.hourly_rate = round(result / self.quan_workers, 2)
 
     def calc_tariff_salary(self) -> None:
-        """
-        Вычисляет тарифную оплату труда = Зтар
-        """
+        """Вычисляет тарифную оплату труда = Зтар"""
         self.tariff_salary = round(self.hourly_rate * constants.EWT * self.quan_workers, 2)
 
     def calc_surch_night_time(self) -> None:
-        """
-        Вычисляет доплату за работу в ночное время = Дн
-        """
-        self.surch_night_time = round(self.hourly_rate * self.quan_workers * constants.NH * 0.4, 2)
+        """Вычисляет доплату за работу в ночное время = Дн"""
+        self.surch_night_time = round(self.hourly_rate * constants.QWNT * constants.NH * 0.4, 2)
 
+    def calc_surch_extended_zone(self) -> None:
+        """Вычисляет доплату за работу по расширенной зоне = Др.з."""
+        self.surch_extended_zone = round(self.hourly_rate * self.quan_workers * constants.EWT * constants.EAS, 2)
+
+    def calc_surch_public_holidays(self) -> None:
+        """Вычисляет доплату за работу в праздничные и выходные дни = Дпр.в"""
+        self.surch_public_holidays = round(self.hourly_rate * constants.QWPH * constants.PH, 2)
+
+    def calc_surch_work_experience(self) -> None:
+        """Вычисляет доплату за стаж работы = Дст.р."""
+        self.surch_work_experience = round(constants.WEA * self.tariff_salary, 2)
+
+    def calc_amount_surchs(self) -> None:
+        """Вычисляет сумму всех доплат = Д"""
+        self.amount_surchs = round(
+            self.surch_night_time + self.surch_extended_zone + \
+            self.surch_public_holidays + self.surch_work_experience, 2
+        )
+
+    def calc_direct_wages(self) -> None:
+        """Вычисляет прямую оплату труда = Зпр"""
+        self.direct_wages = round(self.tariff_salary + self.amount_surchs, 2)
+
+    def calc_amount_premium(self) -> None:
+        """Вычисляет сумму премий = Пр"""
+        self.amount_premium = round(self.direct_wages * constants.PP, 2)
+
+    def calc_amount_surchs_DC(self) -> None:
+        """Вычисляет сумму выплат по районному коэффициенту = РК"""
+        self.amount_surchs_DC = round((self.direct_wages + self.amount_premium) * constants.DC, 2)
+
+    def calc_amount_surchs_NA(self) -> None:
+        """Вычисляет сумму выплат по северной надбавки = СН"""
+        self.amount_surchs_NA = round((self.direct_wages + self.amount_premium) * constants.NA, 2)
+
+    def calc_basic_salary(self) -> None:
+        """Вычисляет основную оплату труда = Зосн"""
+        self.basic_salary = round(
+            self.direct_wages + self.amount_premium + \
+            self.amount_surchs_DC + self.amount_surchs_NA, 2
+        )
+
+    def calc_additional_salary(self) -> None:
+        """Вычисляет дополнительную оплату труда = Здоп"""
+        self.additional_salary = round(self.basic_salary * constants.AS, 2)
+
+    def calc_total_salary(self) -> None:
+        """Вычисляет общую зарплату = Зобщ"""
+        self.total_salary = round(self.basic_salary + self.additional_salary, 2)
+
+    def calc_amount_salary(self) -> None:
+        """Вычисляет среднемесячную зарплату = Зср.м"""
+        self.amount_salary = round(self.total_salary / (12 * self.quan_workers), 2)
--- tests.py
+++ tests.py
@@ -4,25 +4,31 @@ from constants import *
 from unittest import TestCase, main
 
 
-class FormulaObjectTest(TestCase):
+# workers = Worker(
+#     basic_worker=(
+#         BasicWorker('Оператор по ДНГ', 6, 66.2),
+#         BasicWorker('Оператор по ДНГ', 8, 68.7),
+#         BasicWorker('Оператор по ДНГ', 8, 69.9),
+#     ),
+#     auxiliary_worker=(
+#         AuxiliaryWorker('Слесарь', 2, 54.7),
+#         AuxiliaryWorker('Сварщик', 3, 54.9),
+#     ),
+#     et_worker=(
+#         ETWorker('Мастер', 1, 41500)
+#     )
+# )
+
+class TestFormulaByBasicWorker(TestCase):
+    """Тест класса Formula по расчёту основных рабочих"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        self.workers = Worker(
-            basic_worker=(
-                BasicWorker('Оператор по ДНГ', 6, 66.2),
-                BasicWorker('Оператор по ДНГ', 8, 68.7),
-                BasicWorker('Оператор по ДНГ', 8, 69.9),
-            ),
-            auxiliary_worker=(
-                AuxiliaryWorker('Слесарь', 2, 54.7),
-                AuxiliaryWorker('Сварщик', 3, 54.9),
-            ),
-            et_worker=(
-                ETWorker('Мастер', 1, 41500)
-            )
+        workers = (
+            BasicWorker('Оператор по ДНГ', 6, 66.2),
+            BasicWorker('Оператор по ДНГ', 8, 68.7),
+            BasicWorker('Оператор по ДНГ', 8, 69.9),
         )
-        self.formulas = Formula(self.workers.basic_worker)
+        self.formulas = Formula(workers=workers)
 
     def test_calc_hourly_rate(self):
         self.formulas.calc_hourly_rate()
@@ -35,9 +41,161 @@ class FormulaObjectTest(TestCase):
 
     def test_calc_surch_night_time(self):
         self.formulas.calc_hourly_rate()
-        print(self.formulas.surch_night_time)
+        self.formulas.calc_surch_night_time()
+        self.assertTrue(self.formulas.surch_night_time == 49284.0)
+
+    def test_calc_surch_extended_zone(self):
+        self.formulas.calc_hourly_rate()
+        self.formulas.calc_surch_extended_zone()
+        self.assertTrue(self.formulas.surch_extended_zone == 466829.0)
+
+    def test_calc_surch_public_holidays(self):
+        self.formulas.calc_hourly_rate()
+        self.formulas.calc_surch_public_holidays()
+        self.assertTrue(self.formulas.surch_public_holidays == 28338.3)
+
+    def test_calc_surch_work_experience(self):
+        self.formulas.calc_hourly_rate()
+        self.formulas.calc_tariff_salary()
+        self.formulas.calc_surch_work_experience()
+        self.assertTrue(self.formulas.surch_work_experience == 536853.35)
+
+    def test_calc_amount_surchs(self):
+        self.formulas.calc_hourly_rate()
+        self.formulas.calc_tariff_salary()
+        self.formulas.calc_surch_night_time()
+        self.formulas.calc_surch_extended_zone()
+        self.formulas.calc_surch_public_holidays()
+        self.formulas.calc_surch_work_experience()
+        self.formulas.calc_amount_surchs()
+        self.assertTrue(self.formulas.amount_surchs == 1081304.65)
+
+    def test_calc_direct_wages(self):
+        self.formulas.calc_hourly_rate()
+        self.formulas.calc_tariff_salary()
+        self.formulas.calc_surch_night_time()
+        self.formulas.calc_surch_extended_zone()
+        self.formulas.calc_surch_public_holidays()
+        self.formulas.calc_surch_work_experience()
+        self.formulas.calc_amount_surchs()
+        self.formulas.calc_direct_wages()
+        self.assertTrue(self.formulas.direct_wages == 3415449.65)
+
+    def test_calc_amount_premium(self):
+        self.formulas.calc_hourly_rate()
+        self.formulas.calc_tariff_salary()
+        self.formulas.calc_surch_night_time()
+        self.formulas.calc_surch_extended_zone()
+        self.formulas.calc_surch_public_holidays()
+        self.formulas.calc_surch_work_experience()
+        self.formulas.calc_amount_surchs()
+        self.formulas.calc_direct_wages()
+        self.formulas.calc_amount_premium()
+        self.assertTrue(self.formulas.amount_premium == 2049269.79)
+
+    def test_calc_amount_surchs_DC(self):
+        self.formulas.calc_hourly_rate()
+        self.formulas.calc_tariff_salary()
+        self.formulas.calc_surch_night_time()
+        self.formulas.calc_surch_extended_zone()
+        self.formulas.calc_surch_public_holidays()
+        self.formulas.calc_surch_work_experience()
+        self.formulas.calc_amount_surchs()
+        self.formulas.calc_direct_wages()
+        self.formulas.calc_amount_premium()
+        self.formulas.calc_amount_surchs_DC()
+        self.assertTrue(self.formulas.amount_surchs_DC == 3825303.61)
+
+    def test_calc_amount_surchs_NA(self):
+        self.formulas.calc_hourly_rate()
+        self.formulas.calc_tariff_salary()
+        self.formulas.calc_surch_night_time()
+        self.formulas.calc_surch_extended_zone()
+        self.formulas.calc_surch_public_holidays()
+        self.formulas.calc_surch_work_experience()
+        self.formulas.calc_amount_surchs()
+        self.formulas.calc_direct_wages()
+        self.formulas.calc_amount_premium()
+        self.formulas.calc_amount_surchs_NA()
+        self.assertTrue(self.formulas.amount_surchs_NA == 4371775.55)
+
+    def test_calc_basic_salary(self):
+        self.formulas.calc_hourly_rate()
+        self.formulas.calc_tariff_salary()
+        self.formulas.calc_surch_night_time()
+        self.formulas.calc_surch_extended_zone()
+        self.formulas.calc_surch_public_holidays()
+        self.formulas.calc_surch_work_experience()
+        self.formulas.calc_amount_surchs()
+        self.formulas.calc_direct_wages()
+        self.formulas.calc_amount_premium()
+        self.formulas.calc_amount_surchs_DC()
+        self.formulas.calc_amount_surchs_NA()
+        self.formulas.calc_basic_salary()
+        self.assertTrue(self.formulas.basic_salary == 13661798.6)
+
+    def test_calc_additional_salary(self):
+        self.formulas.calc_hourly_rate()
+        self.formulas.calc_tariff_salary()
+        self.formulas.calc_surch_night_time()
+        self.formulas.calc_surch_extended_zone()
+        self.formulas.calc_surch_public_holidays()
+        self.formulas.calc_surch_work_experience()
+        self.formulas.calc_amount_surchs()
+        self.formulas.calc_direct_wages()
+        self.formulas.calc_amount_premium()
+        self.formulas.calc_amount_surchs_DC()
+        self.formulas.calc_amount_surchs_NA()
+        self.formulas.calc_basic_salary()
+        self.formulas.calc_additional_salary()
+        self.assertTrue(self.formulas.additional_salary == 2459123.75)
+
+    def test_calc_total_salary(self):
+        self.formulas.calc_hourly_rate()
+        self.formulas.calc_tariff_salary()
+        self.formulas.calc_surch_night_time()
+        self.formulas.calc_surch_extended_zone()
+        self.formulas.calc_surch_public_holidays()
+        self.formulas.calc_surch_work_experience()
+        self.formulas.calc_amount_surchs()
+        self.formulas.calc_direct_wages()
+        self.formulas.calc_amount_premium()
+        self.formulas.calc_amount_surchs_DC()
+        self.formulas.calc_amount_surchs_NA()
+        self.formulas.calc_basic_salary()
+        self.formulas.calc_additional_salary()
+        self.formulas.calc_total_salary()
+        self.assertTrue(self.formulas.total_salary == 16120922.35)
+
+    def test_calc_amount_salary(self):
+        self.formulas.calc_hourly_rate()
+        self.formulas.calc_tariff_salary()
+        self.formulas.calc_surch_night_time()
+        self.formulas.calc_surch_extended_zone()
+        self.formulas.calc_surch_public_holidays()
+        self.formulas.calc_surch_work_experience()
+        self.formulas.calc_amount_surchs()
+        self.formulas.calc_direct_wages()
+        self.formulas.calc_amount_premium()
+        self.formulas.calc_amount_surchs_DC()
+        self.formulas.calc_amount_surchs_NA()
+        self.formulas.calc_basic_salary()
+        self.formulas.calc_additional_salary()
+        self.formulas.calc_total_salary()
+        self.formulas.calc_amount_salary()
+        self.assertTrue(self.formulas.amount_salary == 61064.1)
 
 
+class TestFormulaByAuxiliaryWorker(TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        workers = (
+            AuxiliaryWorker('Слесарь', 2, 54.7),
+            AuxiliaryWorker('Сварщик', 3, 54.9),
+        )
+        self.formulas = Formula(workers=workers)
+
+    
 
 
 if __name__ == '__main__':
